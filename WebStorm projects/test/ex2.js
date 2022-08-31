5bfdd7971a37c78f888d5be59bbfd1a1448240e3
Bkanycty <kiselyevsk@gmail.com>
2022-09-01 00:37:10 +0700

    D 10.9

--- WebStorm projects/test/ex2.js
+++ WebStorm projects/test/ex2.js
@@ -1,6 +1,13 @@
-const temperature = Number(prompt('Введите температуру по Цельсию'))
-if (isNaN(temperature) === false) {
-    console.log(`${temperature} градусов по Цельсию равны ${((temperature * 9/5) + 32)} градусам по Фаренгейту`)
-} else {
-    console.log('Введите числовое значение')
-}
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "gray";
+ctx.fillRect(0, 0, 300, 300);
+
+// Полый прямоугольник
+ctx.strokeStyle = "yellow"; // Цвет границ прямоугольника
+ctx.strokeRect(10, 20, 30, 40); // Метод отрисовки полого прямоугольника
+
+// Залитый прямоугольник
+ctx.fillStyle = "green"; // Цвет залитого прямоугольника
+ctx.fillRect(70, 10, 50, 50); // Метод отрисовки залитого прямоугольника
--- WebStorm projects/test/ex3.js
+++ WebStorm projects/test/ex3.js
@@ -1,3 +1,9 @@
-const x = Number(prompt('Введите числовое значение x'))
-y = ((4 * x ** 2) + 18 - (23 * x)) / ((5 * x / 9) + (18 * (x ** 3 / 33 * x))) + (x * 15 / 12)
-alert(y)
+const tri = document.getElementById("tri");
+const tri_ctx = tri.getContext("2d");
+
+tri_ctx.beginPath(); // Создает новый контур
+tri_ctx.moveTo(75,50); // Перемещает перо в точку с координатами x и y.
+tri_ctx.lineTo(100,75); // Рисует линию с текущей позиции до позиции, определенной x и y.
+tri_ctx.lineTo(100,25);
+tri_ctx.fillStyle = "green"; // Заливаем выбранным цветом фигуру
+tri_ctx.fill(); // Рисует фигуру с заливкой внутренней области.
