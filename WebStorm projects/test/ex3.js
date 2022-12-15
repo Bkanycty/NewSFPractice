db149a052574e4262fbc2011948d5291f5fb7586
Bkanycty <kiselyevsk@gmail.com>
2022-12-16 00:08:48 +0700

    213

--- WebStorm projects/test/ex3.js
+++
@@ -1,9 +0,0 @@
-const tri = document.getElementById("tri");
-const tri_ctx = tri.getContext("2d");
-
-tri_ctx.beginPath(); // Создает новый контур
-tri_ctx.moveTo(75,50); // Перемещает перо в точку с координатами x и y.
-tri_ctx.lineTo(100,75); // Рисует линию с текущей позиции до позиции, определенной x и y.
-tri_ctx.lineTo(100,25);
-tri_ctx.fillStyle = "green"; // Заливаем выбранным цветом фигуру
-tri_ctx.fill(); // Рисует фигуру с заливкой внутренней области.
